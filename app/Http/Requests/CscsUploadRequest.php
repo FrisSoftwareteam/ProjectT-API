1c0997ab49e0b883c0d95792f222e1040543f01e
Esan Joshua <joshuaesan1@gmail.com>
2026-04-21 09:31:43 +0100

    CSCS upload

--- app/Http/Requests/CscsUploadRequest.php
+++ app/Http/Requests/CscsUploadRequest.php
@@ -3,12 +3,28 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\UploadedFile;
 
 class CscsUploadRequest extends FormRequest
 {
     public function authorize(): bool
     {
         return true;
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $files = $this->file('files');
+
+        if ($files instanceof UploadedFile) {
+            $this->files->set('files', [$files]);
+
+            return;
+        }
+
+        if (is_array($files)) {
+            $this->files->set('files', array_values(array_filter($files)));
+        }
     }
 
     public function rules(): array
